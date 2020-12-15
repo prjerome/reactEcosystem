cc75705988e21138b02dc34e74a37d52f4a17bbd
Pradeep Jerome <prjerome@gmail.com>
2020-12-15 19:26:39 +0100

    with useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const title = 'Hello World';
 
 function App () {
     const [count, setCount] = useState(0);
+
+    useEffect(() => document.title = count)
+
     return(
     <div>
         {title}
