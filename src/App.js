81a25fbf02944192ddf79e8e2601db0a102a52f7
Pradeep Jerome <prjerome@gmail.com>
2020-12-16 11:23:52 +0100

    Small demo app using useState and useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { useState, useEffect } from 'react';
 
-const title = 'Hello World';
+const title = 'React.js hooks demo app.';
 
 function App () {
     const [count, setCount] = useState(0);
 
+    const [divColor, setDivColor] = useState('');
+
     useEffect(() => document.title = count)
 
+    const handleChange = ({target}) => setDivColor(target.value) ;
+
     return(
-    <div>
+    <div style={{ background:divColor }}>
         {title}
-        <button onClick={() => setCount(count +1 )}>{count}</button>
+        <input style={{display:'block'}}  type="text" placeholder="Type in a color eg cyan" onChange={handleChange} />
+        
+        <button style={{display:'block'}} onClick={() => setCount(count + 1 )}>Click me and observe the tab title change</button>
     </div>)
-};
+};  
 
 export default App;
