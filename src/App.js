1faabdbec314533d290232987066d5401632ef2f
Pradeep Jerome <prjerome@gmail.com>
2020-12-17 15:01:19 +0100

    Added App.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './App.css';
 
 const title = 'React.js hooks demo app.';
 
@@ -12,12 +13,18 @@ function App () {
     const handleChange = ({target}) => setDivColor(target.value) ;
 
     return(
-    <div style={{ background:divColor }}>
-        {title}
-        <input style={{display:'block'}}  type="text" placeholder="Type in a color eg cyan" onChange={handleChange} />
+    <div>
+         <header> <h1>{title}</h1> </header> 
+       <div className="container" style={{ background:divColor }}>
+        <section>
+            <input className="input" style={{display:'block'}}  type="text" placeholder="Type in a color eg cyan" onChange={handleChange} />
+            
+            <button style={{display:'block'}} onClick={() => setCount(count + 1 )}>Click me and observe the browser tab title change</button>
+        </section>
         
-        <button style={{display:'block'}} onClick={() => setCount(count + 1 )}>Click me and observe the tab title change</button>
-    </div>)
+    </div>
+    </div>
+      )
 };  
 
 export default App;
