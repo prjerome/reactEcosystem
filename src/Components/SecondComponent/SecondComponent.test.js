77b7d907f30bc5d75bc8d2edfdf332f91f09394f
Pradeep Jerome <prjerome@gmail.com>
2020-12-19 16:32:33 +0100

    Added queryByText assertion

--- src/Components/SecondComponent/SecondComponent.test.js
+++ src/Components/SecondComponent/SecondComponent.test.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import SecondComponent from './SecondComponent';
 
@@ -11,11 +11,21 @@ test('given there is a SecondComponent, when it renders, then it does not throw'
     });
 
     test('given when the Second Component renders, then the screen contains the text Hello World', () => {
-        render(<SecondComponent />);
+    render(<SecondComponent />);
         expect(screen.getByText('Hello World')).toBeInTheDocument();
     });
 
-    test('given when the Second Component renders, then the text within it is visible with h1 styling', () => {
+    test('given when the Second Component renders, then the text is within a button', () => {
         render(<SecondComponent />);
-        screen.getByRole(' ', { hidden: true });
+        //now an explicit test, instead of implicit. Compare with prev commit for diff.
+        expect(screen.getByRole('button', { hidden: true })).toBeInTheDocument();
     });
+
+    test('given when the Second Component renders, then the screen does not contain the text "Goodbye World"', () => {
+        // Arrange and Act
+        render(<SecondComponent />);
+
+        // Assert
+        expect(screen.queryByText('Goodbye World')).toBeNull();
+    });
+    
