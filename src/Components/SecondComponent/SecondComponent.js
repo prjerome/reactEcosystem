8d6b0374c086cc8f59b708e9f23140200168855a
Pradeep Jerome <prjerome@gmail.com>
2020-12-30 08:47:34 +0100

    Committing  before reseting to previous commit

--- src/Components/SecondComponent/SecondComponent.js
+++ src/Components/SecondComponent/SecondComponent.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-function SecondComponent () {
-  const [signIn, setSignIn] = useState('Sign in')
+function SecondComponent() {
+  const [signIn, setSignIn] = useState("Sign in");
 
   return (
     <div>
       <h1>Hello World</h1>
-      <button onClick={() => setSignIn('Signed in as')}>{signIn}</button>
+      <button onClick={() => setSignIn("Signed in as")}>{signIn}</button>
     </div>
-  )
+  );
 }
 
-export default SecondComponent
+export default SecondComponent;
